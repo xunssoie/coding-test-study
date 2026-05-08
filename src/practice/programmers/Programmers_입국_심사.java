f52e482101e856a9962bea1e4a6f75609b748699
xunxxoie <ahh0520@naver.com>
2026-05-09 00:32:09 +0900

    feat: Programmers_입국_심사

---
+++ src/practice/programmers/Programmers_입국_심사.java
@@ -0,0 +1,45 @@
+package practice.programmers;
+
+public class Programmers_입국_심사{
+    public static void main(String[] args) {
+        System.out.println("solution = " + solution(6, new int[]{7,10}));
+    }
+
+    public static long solution(int n, int[] times){
+        int max = times[0];
+        for (int time : times) {
+            if (time > max) {
+                max = time;
+            }
+        }
+
+        long low = 0;
+        long high = (long) max * n;
+
+        while(low < high){
+            long mid = low + (high - low) / 2;
+
+            if(canFinish(n, mid, times)){
+                high = mid;
+            }else{
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+
+    public static boolean canFinish(int n, long t, int[] times){
+        long sum = 0;
+        for (int time : times) {
+            sum += t / time;
+        }
+
+        if (sum >= n){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+
+}
