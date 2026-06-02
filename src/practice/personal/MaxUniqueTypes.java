262e749ed85b06e41e63b180f3a32824bbd40799
xunxxoie <ahh0520@naver.com>
2026-06-02 22:36:51 +0900

    feat: MaxUniqueTypes

---
+++ src/practice/personal/MaxUniqueTypes.java
@@ -0,0 +1,22 @@
+package practice.personal;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class MaxUniqueTypes {
+    public static void main(String[] args) {
+        System.out.println(solution(new int[]{3,1,2,3}));
+        System.out.println(solution(new int[]{3,3,3,2,2,4}));
+        System.out.println(solution(new int[]{3,3,3,2,2,2}));
+    }
+
+    public static int solution(int[] nums){
+        Set<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+            set.add(num);
+        }
+
+        return Math.min(set.size(), nums.length/2);
+    }
+}
