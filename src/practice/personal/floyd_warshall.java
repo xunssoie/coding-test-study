91047611d68a9b9d1b069006c7d653eb7dbb0583
xunxxoie <ahh0520@naver.com>
2026-07-08 20:58:34 +0900

    feat: floyd_warshall

---
+++ src/practice/personal/floyd_warshall.java
@@ -0,0 +1,44 @@
+package practice.personal;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class floyd_warshall {
+    static final int INF = 1_000_000_000;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int V = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
+
+        int[][] dist = new int[V+1][V+1];
+
+        for(int i = 1; i <= V; i++){
+            Arrays.fill(dist[i], INF);
+            dist[i][i] = 0;
+        }
+
+        for(int e = 0; e < E; e++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+
+            dist[a][b] = Math.min(dist[a][b], w);
+        }
+
+        for(int k = 1; k <= V; k++){
+            for(int i = 1; i <= V; i++){
+                for(int j = 1; j <= V; j++){
+                    if(dist[i][k] + dist[k][j] < dist[i][j])
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                }
+            }
+        }
+    }
+}
